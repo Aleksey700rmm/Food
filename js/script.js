f004c8d54b1f7aa6491c31a673a7c9b00cf17386
Aleksey700rmm <92338644+Aleksey700rmm@users.noreply.github.com>
2022-03-20 22:52:32 +0300

    Food without comments(59)

--- js/script.js
+++ js/script.js
@@ -108,14 +108,6 @@ window.addEventListener('DOMContentLoaded', () => {
     // clearInterval(modalTimerId); //Чтобы отключить таймер если окно уже открывалсоь
     }
 
-    // modalTrigger.forEach(btn => {
-    //     btn.addEventListener('click', () => {
-    //         modal.classList.add('show');
-    //         modal.classList.remove('hide');
-    //         // modal.classList.toggle('show');
-    //         document.body.style.overflow = 'hidden';  //чтобы отключить скролл когда окно открыто
-    //     });
-    // });
     modalTrigger.forEach(btn => {
         btn.addEventListener('click', openModal);
     });
@@ -126,13 +118,6 @@ window.addEventListener('DOMContentLoaded', () => {
         // modal.classList.toggle('show');
         document.body.style.overflow = '';
     }
-    
-    // modalCloseBtn.addEventListener('click', () => {
-    //     modal.classList.remove('show');
-    //     modal.classList.add('hide');
-    //     // modal.classList.toggle('show');
-    //     document.body.style.overflow = '';
-    // });
 
     modal.addEventListener('click', (e) => {
         if (e.target === modal || e.target.getAttribute('data-close') == '') {
@@ -217,42 +202,6 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         });
 
-    // new menuCard(
-    //     "img/tabs/vegy.jpg",
-    //     "vegy",
-    //     'Меню "Фитнес"',
-    //     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    //     9,
-    //     '.menu .container',
-    //     'menu__item',
-    //     'big'
-
-    // ).render();
-
-    // new menuCard(
-    //     "img/tabs/elite.jpg",
-    //     "elite",
-    //     'Меню “Премиум”',
-    //     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    //     14,
-    //     '.menu .container',
-    //     'menu__item',
-    //     'big'
-
-    // ).render();
-
-    // new menuCard(
-    //     "img/tabs/post.jpg",
-    //     "post",
-    //     'Меню "Постное"',
-    //     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    //     21,
-    //     '.menu .container',
-    //     'menu__item',
-    //     'big'
-
-    // ).render();
-
     // Forms
 
     const forms = document.querySelectorAll('form');
@@ -294,23 +243,8 @@ window.addEventListener('DOMContentLoaded', () => {
             // request.setRequestHeader('Content-type', 'application/json');//Когда мы используем XMLHttpRequest и FormData заголовок не нужен
             const formData = new FormData(form);
 
-            // const object = {};
-            // formData.forEach(function(value, key) {
-            //     object[key] = value;
-            // });
-
             const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            // const obj ={a: 23, b: 50};
-            // console.log(Object.entries(obj));
-
-            // fetch('server.php', {
-            //     method: 'POST',
-            //     headers: {
-            //         'Content-type': 'application/json'
-            //     },
-            //     body: JSON.stringify(object)
-            // });
             postData('http://localhost:3000/requests', json)
             .then(data => {
                 console.log(data);
@@ -321,17 +255,6 @@ window.addEventListener('DOMContentLoaded', () => {
             }).finally(() => {
                 form.reset();
             });
-
-            // request.addEventListener('load', () => {
-            //     if (request.status === 200) {
-            //         console.log(request.response);
-            //         showThanksModal(message.success);
-            //         form.reset();                   
-            //         statusMessage.remove();
-            //     } else {
-            //         showThanksModal(message.failure);
-            //     }
-            // });
         });
     }
 
@@ -358,18 +281,4 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
-
-    // fetch('http://localhost:3000/menu')
-    //     .then(data => data.json())
-    //     .then(res => console.log(res));
-
-    // fetch('https://jsonplaceholder.typicode.com/posts', {
-    //     method: 'POST',
-    //     body: JSON.stringify({name: 'Alex'}),
-    //     headers: {
-    //         'Content-type': 'application/json'
-    //     }        
-    // })
-    // .then(response => response.json())
-    // .then(json => console.log(json));
 });
